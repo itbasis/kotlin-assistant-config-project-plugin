1ce2da442e46e7df91cf19f93431aed581c6dfdb
Victor Alenkov <victor@alenkov.name>
2018-11-03 18:31:21 +0300

    * Upgraded Gradle version * Up Java version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,6 @@
 import org.gradle.api.tasks.wrapper.Wrapper.DistributionType
 
 tasks.withType(Wrapper::class.java).configureEach {
-  gradleVersion = "5.0-milestone-1"
+  gradleVersion = "5.0-rc-1"
   distributionType = DistributionType.ALL
 }
